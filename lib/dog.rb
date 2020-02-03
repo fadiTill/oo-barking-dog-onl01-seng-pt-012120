3b932cb47520bc40bbf72a9984e678dd584c262b
fadimata tillman <fadi.tillman@gmail.com>
2020-02-03 21:46:07 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,14 +1,21 @@
 
 
 class Dog
+  def bark=(dogs_name)
+    @this_dog_name = dogs_bark
+  end
 
+  def name
+    @this_dog_name
+  end
+  end
 
   def bark=(dogs_bark)
-    @this_dog_bark = dogs_bark
+    @this_dog_name = dogs_bark
   end
 
   def bark
-    @this_dog_bark
+    @this_dog_name
   end
 end
 
